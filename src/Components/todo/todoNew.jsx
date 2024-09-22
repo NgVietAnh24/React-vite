8bfc1fa2a2c3036e199cbdc434aea4c321eed29f
VietAnh244 <117790397+NgVietAnh24@users.noreply.github.com>
2024-09-22 12:26:57 +0700

    Video 36

--- src/Components/todo/todoNew.jsx
+++ src/Components/todo/todoNew.jsx
@@ -3,11 +3,17 @@ import '../todo/todo.css'
 const TodoNew = (props) => {
     console.log(">>>>> Check props todoNew: ", props);
     const { addNewData } = props;
-    addNewData("Viet Anh")
+    // addNewData("Viet Anh")
+    const handleClick = () => {
+        alert(">>>>> Click me 🔥🔥🔥");
+    }
+    const handleOnChange = (name) => {
+        console.log(">>>>> handleOnChange 🔥🔥🔥", name);
+    }
     return (
         <div className='todo-new'>
-            <input className='input-task' type="text" placeholder='Enter your task' />
-            <button className='btn-add'>Add</button>
+            <input className='input-task' onChange={(event) => handleOnChange(event.target.value)} type="text" placeholder='Enter your task' />
+            <button className='btn-add' style={{ cursor: "pointer" }} onClick={() => handleClick()}>Add</button>
         </div>
     );
 }
